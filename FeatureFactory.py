4b0106e0a4453302f1b4d8a4b5c24a4117004d64
buaahsh <buaahsh@foxmail.com>
2015-11-24 21:57:37 +0800

    add FeatureFactory

---
+++ FeatureFactory.py
@@ -0,0 +1,114 @@
+# -*- coding:utf-8 -*-
+
+
+import argparse
+from collections import Counter
+
+
+class FlowV5():
+
+    def __init__(self, tokens):
+        self.srcAdd = tokens[0]
+        self.srcPort = int(tokens[1])
+        self.dstAdd = tokens[2]
+        self.dstPort = int(tokens[3])
+        self.tcpFlag = int(tokens[4])
+        self.prot = int(tokens[5])
+        self.pkts = int(tokens[6])
+        self.octs = int(tokens[7])
+        self.isSyn = self.tcpFlag & 2
+        self.isIcmp = 0
+        if self.prot == 1:
+            self.isIcmp = 1
+
+
+def outputFeaure(outputFile, _dict, isSourceKey):
+    """
+    Convert into features, the format of feature is:
+    key,isSourceKey,
+    nSrcAdds,nSrcPorts,nDstAdds,nDstPorts
+    nFlows,nPkts,avgFlowsSize,avgPktSize,nICMP/nFlows,nICMP/nPkts,nSYN/nFlows
+    """
+    def convert2feaure(flows):
+
+        nSrcAdds = {}
+        nSrcPorts = {}
+        nDstAdds = {}
+        nDstPorts = {}
+        nFlows = 0
+        nPkts = 0
+        avgFlowsSize = 0
+        avgPktSize = 0
+        nICMPFlow = 0
+        nICMPPkt = 0
+        nSYN = 0
+        for f in flows:
+            if f.srcAdd not in nSrcAdds:
+                nSrcAdds[f.srcAdd] = 1
+            if f.dstAdd not in nDstAdds:
+                nDstAdds[f.dstAdd] = 1
+            if f.srcAdd + "," + str(f.srcPort) not in nSrcPorts:
+                nSrcPorts[f.srcAdd + "," + str(f.srcPort)] = 1
+            if f.dstAdd + "," + str(f.dstPort) not in nDstPorts:
+                nDstPorts[f.dstAdd + "," + str(f.dstPort)] = 1
+            nFlows += 1
+            nPkts += f.pkts
+            nICMPFlow += f.isIcmp
+            nICMPPkt += f.isIcmp * f.pkts
+            nSYN += f.isSyn
+            avgFlowsSize += (f.octs - avgFlowsSize) * 1.0 / (nFlows + 1)
+            avgPktSize += (f.octs - avgPktSize) * 1.0 / (nPkts + 1)
+
+        return [len(nSrcAdds), len(nSrcPorts), len(nDstAdds), len(nDstPorts),
+                nFlows, nPkts, avgFlowsSize, avgPktSize,
+                nICMPFlow * 1.0 / nFlows,
+                nICMPPkt * 1.0 / nPkts,
+                nSYN * 1.0 / nFlows]
+
+    for k in _dict:
+        flows = _dict[k]
+        features = convert2feaure(flows)
+        features = [str(f) for f in features]
+        print >>outputFile,  "%s,%s" % (k, ",".join(features))
+
+
+def buildFearueFile(inputFile, outputFile, isSourceKey):
+    _dict = {}
+    num = 0
+    for line in inputFile:
+        num += 1
+        if num % 100000 == 0:
+            print num
+        tokens = line.split(",")
+        if len(tokens) != 8:
+            continue
+        flow = FlowV5(tokens)
+        if flow.pkts < 10:
+            continue
+        key = flow.srcAdd
+        if not isSourceKey:
+            key = flow.dstAdd
+        if key in _dict:
+            _dict[key].append(flow)
+        else:
+            _dict[key] = [flow]
+    outputFeaure(outputFile, _dict, isSourceKey)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="input file")
+    parser.add_argument("-o", "--output", help="output file")
+    parser.add_argument("-k", "--key", help="aggregation key", default="src")
+
+    args = parser.parse_args()
+    print "Args :", args
+    inputFile = args.input
+    outputFile = args.output
+    isSourceKey = True
+    if args.key != "src":
+        isSourceKey = False
+
+    with open(outputFile, 'w') as fOut:
+        with open(inputFile, 'r') as fIn:
+            buildFearueFile(fIn, fOut, isSourceKey)
