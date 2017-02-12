7c9ee47037f53d77bd0305f07359a508cbd1a848
Shaohan Huang <buaahsh@foxmail.com>
2017-02-12 20:41:50 +0800

    add pipeline

--- DBSCAN4AP/Model/Pipeline/Tencent_LDP_pipeline.py
+++ DBSCAN4AP/Model/Pipeline/Tencent_LDP_pipeline.py
@@ -5,16 +5,21 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
 
 from Model.Core.LDP import LDP
+import pandas as pd
 
 
 def get_data(file_name):
-    data = []
-    with open(file_name, 'r') as f_in:
-        for line in f_in:
-            line = line.strip().strip(',')
-            v = float(line)
-            data.append(v)
-    return data
+    dataset = pd.read_csv(file_name)
+    # print dataset['value']
+    # windows_width = 5
+    # data = []
+    return dataset['value']
+    # with open(file_name, 'r') as f_in:
+    #     for line in f_in:
+    #         line = line.strip().strip(',')
+    #         v = float(line)
+    #         data.append(v)
+    # return data
 
 def handle_one_file(file_name, f_out, windows_width):
     dataset = get_data(file_name)
@@ -22,9 +27,9 @@ def handle_one_file(file_name, f_out, windows_width):
     ldp = LDP(windows_width=windows_width)
 
     index = min(1000, len(dataset))
-    labels = ldp.train(dataset[:index], eps=0.3, min_samples=3)
+    labels = ldp.train(dataset[:index], eps=0.3, min_samples=5)
 
-    labels = ldp.predict(dataset)
+    labels = ldp.predict(dataset, 10)
     plot(dataset, labels, file_name)
     # precision, recall = ldp.evaluate(dataset['is_anomaly'], labels)
     # s = "%s\t%f\t%f\t%d" % (file_name, precision, recall, anomaly_num)
@@ -52,17 +57,19 @@ def plot(dataset, labels, file_name):
 
 def pipeline():
     with open("tencent-pipeline.txt", 'w') as f_out:
-        root = "/Users/hsh/Downloads/anomaly_data/Tencent/1d"
+        root = "/Users/hsh/Downloads/anomaly_data/Yahoo/ydata-labeled-time-series-anomalies-v1_0/a1"
         for i in os.listdir(root):
             file_name = os.path.join(root,i)
-            if file_name.endswith("txt"):
-                try:
-                    print(file_name)
-                    handle_one_file(file_name, f_out, 0)
-                    # handle_one_file(file_name, f_out, 3)
-                    # handle_one_file(file_name, f_out, 5)
-                except:
-                    print file_name, "Error!!!!!!!"
+            if file_name.endswith("csv"):
+                handle_one_file(file_name, f_out, 0)
+
+                # try:
+                #     print(file_name)
+                #     handle_one_file(file_name, f_out, 0)
+                #     # handle_one_file(file_name, f_out, 3)
+                #     # handle_one_file(file_name, f_out, 5)
+                # except:
+                #     print file_name, "Error!!!!!!!"
 
 
 if __name__ == "__main__":
